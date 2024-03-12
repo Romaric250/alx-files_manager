aa94b8b4dd7f0f1685b7c9e99f9e4a381142491e
Romaric250 <78319422+Romaric250@users.noreply.github.com>
2024-03-12 10:36:51 +0000

    file controller updated

---
+++ controllers/FileController.js
@@ -0,0 +1,74 @@
+
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const dbClient = require('../utils/db');
+const redisClient = require('../utils/redis');
+
+class FilesController {
+  static async postFile(req, res) {
+    const token = req.headers['x-token'];
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
+
+    const { name, type, parentId = 0, isPublic = false, data } = req.body;
+    if (!name) {
+      res.status(400).json({ error: 'Missing name' });
+      return;
+    }
+    if (!type || !['folder', 'file', 'image'].includes(type)) {
+      res.status(400).json({ error: 'Missing type' });
+      return;
+    }
+    if (!data && type !== 'folder') {
+      res.status(400).json({ error: 'Missing data' });
+      return;
+    }
+
+    if (parentId !== 0) {
+      const parentFile = await dbClient.getFileById(parentId);
+      if (!parentFile) {
+        res.status(400).json({ error: 'Parent not found' });
+        return;
+      }
+      if (parentFile.type !== 'folder') {
+        res.status(400).json({ error: 'Parent is not a folder' });
+        return;
+      }
+    }
+
+    let localPath = null;
+    if (type !== 'folder') {
+      const folderPath = process.env.FOLDER_PATH || '/tmp/files_manager';
+      if (!fs.existsSync(folderPath)) {
+        fs.mkdirSync(folderPath, { recursive: true });
+      }
+
+      localPath = path.join(folderPath, uuidv4());
+      fs.writeFileSync(localPath, data, { encoding: 'base64' });
+    }
+
+    const newFile = await dbClient.createFile({
+      userId,
+      name,
+      type,
+      isPublic,
+      parentId,
+      localPath,
+    });
+
+    res.status(201).json({
+      id: newFile.insertedId,
+      userId,
+      name,
+      type,
+      isPublic,
+      parentId,
+    });
+  }
+}
+
+module.exports = FilesController;
