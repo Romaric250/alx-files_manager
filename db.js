4e699d3019d89d09b6d4c08a67b79ec805b9e24f
Romaric250 <78319422+Romaric250@users.noreply.github.com>
2024-03-12 09:47:14 +0000

    task 1 completed

---
+++ db.js
@@ -0,0 +1,34 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url);
+    this.client.connect();
+
+    this.db = this.client.db(database);
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const collection = this.db.collection('users');
+    const count = await collection.countDocuments();
+    return count;
+  }
+
+  async nbFiles() {
+    const collection = this.db.collection('files');
+    const count = await collection.countDocuments();
+    return count;
+  }
+}
+
+const dbClient = new DBClient();
+module.exports = dbClient;
