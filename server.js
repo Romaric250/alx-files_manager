516a5118362a51daf3eaf6b067088d9f06d697e9
Romaric250 <78319422+Romaric250@users.noreply.github.com>
2024-03-12 17:13:48 +0000

    updated server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
+import express from 'express';
+import routes from './routes/index';
 
-const express = require("express");
-const router = require("./routes/index");
+const app = express();
+app.use(express.json());
+const port = process.env.PORT || 5000;
 
-const server = express();
-const PORT = process.env.PORT ? process.env.PORT : 5000;
+app.use('/', routes);
 
-server.use(express.json());
-server.use(router);
-
-server.listen(PORT, () =>
-  console.log(` Server is running on port: ${PORT}`)
-);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
