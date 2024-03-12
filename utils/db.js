f085cae36ca29b412c916d98b4b5dae2ed2fffc3
Romaric250 <78319422+Romaric250@users.noreply.github.com>
2024-03-12 16:53:51 +0000

    fixed eslintrec issues

--- utils/db.js
+++ utils/db.js
@@ -2,9 +2,9 @@ import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || 'localhost';
-    const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
+    const port = process.env.DB_PORT || 27017;
+    const host = process.env.DB_HOST || 'localhost';
     const url = `mongodb://${host}:${port}`;
 
     MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
