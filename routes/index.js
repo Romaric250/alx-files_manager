d9eec78e1b6b2a8f0263ed74fc27f531bed11277
Romaric250 <78319422+Romaric250@users.noreply.github.com>
2024-03-12 10:45:53 +0000

    updated the name of the file controller

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,13 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
+
 
 const router = express.Router();
 
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 router.post('/users', UsersController.postNew);
 router.get('/status', AppController.getStatus);
 router.get('/connect', AuthController.getConnect);
